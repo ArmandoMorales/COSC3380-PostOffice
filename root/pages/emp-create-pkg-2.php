14f7bbfa4844569797555258713248d5f91907ba
Zachary Hoover <zahoover@gmail.com>
2022-04-18 23:40:24 -0500

    Fixed emp-create-pkg In addtion, pkg-3 is now depreceated

--- root/pages/emp-create-pkg-2.php
+++ root/pages/emp-create-pkg-2.php
@@ -1,16 +1,30 @@
 <html>
 <head>
 
-<?php
+<?php 
     include("../includes/dbh.inc.php");
+    $customID = $_POST["customer-id"];
+    $Rbuildingnum = $_POST['Rbuilding-num'];
+    $Rbnum_converted = (int) $Rbuildingnum;
+    $Rstreet = $_POST['Rstreet-name'];
+    $Rcity = $_POST['Rcity'];
+    $Rstate = $_POST['Rstate'];
+    $Rzipcode = $_POST['Rzip'];
+    $Rzipcode_coverted = (int) $Rzipcode;
+                
+    $Dbuildingnum = $_POST['Dbuilding-num'];
+    $Dbnum_converted = (int) $Dbuildingnum;
+    $Dstreet = $_POST['Dstreet-name'];
+    $Dcity = $_POST['Dcity'];
+    $Dstate = $_POST['Dstate'];
+    $Dzipcode = $_POST['Dzip'];
+    $Dzipcode_coverted = (int) $Dzipcode;
+                
+    $ptype = $_POST['ptype'];
+    $weight = $_POST['weight'];
+    $vol = $_POST['vol'];
+    $vol_converted = (int) $vol;
 ?>
-
-<section class="header">
-    <?php
-    include_once '../header.php';
-    ?>
-</section>
-        
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Employee Self Services</title>
@@ -22,10 +36,9 @@
 <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-    <!-- Commented out due to header above
     <section class="sub-header">
         <nav>
-            
+            <!-- TODO: add redirects based on employee permissions -->
             <a href=""><img src="../images/pinkpostlogo.png"></a>
             <div class="nav-links" id="navLinks">  
                 <ul>
@@ -37,14 +50,14 @@
             </div>
         </nav>
             <h1></h1>
-    </section> -->
+    </section>
 
     <!-- Side Bar -->
     <section class="side-bar-container">
         <div class="side-bar" id="sidebar">
             <div class="list">
                 <a href="employee-services.php"><div class="icons"><i class="fa fa-user" aria-hidden="true"></i><p id="icon-txt">Employee Information</p></div></a>
-                <a href="emp-create-pkg-1.php"><div class="icons"><i class="fa fa-dropbox" aria-hidden="true"></i><p id="icon-txt">Start Package</p></div></a>
+                <a href="emp-create-pkg-1.php"><div class="icons"><i class="fa fa-dropbox" aria-hidden="true"></i><p id="icon-txt">Create Package</p></div></a>
                 <a href="emp-update-trk-1.php"><div class="icons"><i class="fa fa-truck" aria-hidden="true"></i><p id="icon-txt">Update Tracking</p></div></a>
                 <a href="emp-update-inv-1.php"><div class="icons"><i class="fa fa-book" aria-hidden="true"></i><p id="icon-txt">Update Inventory</p></div></a>
             </div>
@@ -52,206 +65,165 @@
 
         <!-- content -->
         <div class="content">
+            <?php
+                $addrRKeys = "SELECT Address_Key FROM PostOffice.Address 
+                                WHERE Building_Num = ? AND Street_Name = ? AND City = ? AND State = ? AND Zipcode = ?;";
+                
+                $stmtRKeys = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmtRKeys, $addrRKeys)){
+                    header("location: ../pages/index-login.php?error=stmtfailed");
+                    exit();   
+                }
+                mysqli_stmt_bind_param($stmtRKeys, "isssi", $Rbnum_converted, $Rstreet, $Rcity, $Rstate, $Rzipcode_coverted );
+                mysqli_stmt_execute($stmtRKeys);
 
-        <?php
-        
-        //Make sure input Package_ID is valid 
-        $pkgID = $_POST['package-id'];
-        $pkgIDConverted = (int) $pkgID;
-        
+                $resultRKey = mysqli_stmt_get_result($stmtRKeys);
+                $resultRKeyCheck = mysqli_num_rows($resultRKey);
+                
+                $holdRaddr = -1;
+                
+                if($resultRKeyCheck > 0) {
+                    while($resultRKeyCheck = mysqli_fetch_assoc($resultRKey))
+                    {
+                        $holdRaddr = $resultRKeyCheck["Address_Key"];       
+                    }
+                } else {
+                    $sqlRaddr = "INSERT INTO PostOffice.Address (Building_Num, Street_Name, City, State, Zipcode)
+                    VALUES (?, ?, ?, ?, ?);";
+                    $stmtRaddr = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmtRaddr, $sqlRaddr)) {
+                        header("location: ../pages/index-login.php?error=stmtfailed");
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmtRaddr, "isssi", $Rbnum_converted, $Rstreet, $Rcity, $Rstate, $Rzipcode_coverted );
+                    mysqli_stmt_execute($stmtRaddr);
+                
+                    $addrRKeys = "SELECT Address_Key FROM PostOffice.Address 
+                                WHERE Building_Num = ? AND Street_Name = ? AND City = ? AND State = ? AND Zipcode = ?;";
+                
+                    $stmtRKeys = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmtRKeys, $addrRKeys)) {
+                        header("location: ../pages/index-login.php?error=stmtfailed");
+                        exit();   
+                    }
+                    mysqli_stmt_bind_param($stmtRKeys, "isssi", $Rbnum_converted, $Rstreet, $Rcity, $Rstate, $Rzipcode_coverted );
+                    mysqli_stmt_execute($stmtRKeys);
 
-        $pkgsql = "SELECT * FROM PostOffice.Package WHERE PACKAGE_ID = ?;";
-        $stmtpkg = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmtpkg, $pkgsql))
-        {
-            header("location: ../pages/index-login.php?error=stmtfailed");
-            exit();
-        }
-        mysqli_stmt_bind_param($stmtpkg, "i", $pkgID);
-        mysqli_stmt_execute($stmtpkg);
+                    $resultRKey = mysqli_stmt_get_result($stmtRKeys);
+                    $resultRKeyCheck = mysqli_num_rows($resultRKey);
+                
+                    $holdRaddr = -1;
+                
+                    if($resultRKeyCheck > 0) {
+                        while($resultRKeyCheck = mysqli_fetch_assoc($resultRKey)) {
+                            $holdRaddr = $resultRKeyCheck["Address_Key"];       
+                        }
+                    }
+                    else {
+                    echo "error";
+                    }
+                }
 
-        $pkgStartRow = mysqli_stmt_get_result($stmtpkg);
-        $pkgStartRowCheck = mysqli_num_rows($pkgStartRow);
 
-        $holdPkgKey = -1;
-        $holdPkgWeight = -1;
-        $holdPkgVol = -1;
-        
+                //Retrieve Destination Destination Address Keys
+                $addrDKeys = "SELECT Address_Key FROM PostOffice.Address 
+                                WHERE Building_Num = ? AND Street_Name = ? AND City = ? AND State = ? AND Zipcode = ?;";
+                
+                $stmtDKeys = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmtDKeys, $addrDKeys)) {
+                    header("location: ../pages/index-login.php?error=stmtfailed");
+                    exit();   
+                }
+                mysqli_stmt_bind_param($stmtDKeys, "isssi", $Dbnum_converted, $Dstreet, $Dcity, $Dstate, $Dzipcode_coverted );
+                mysqli_stmt_execute($stmtDKeys);
 
-        if ($pkgStartRowCheck > 0)
-        { 
-            //This will be used and get deleted later in emp-create-pjg-3.php
-            $_SESSION['packageIDglobal'] = $pkgIDConverted;
-         
-            while($pkgStartRowCheck = mysqli_fetch_assoc($pkgStartRow))
-            {
-                $holdPkgKey = $pkgStartRowCheck["Package_ID"];
-                $holdPkgWeight = $pkgStartRowCheck["Package_Weight"];     
-                $holdPkgVol = $pkgStartRowCheck["Package_Volume"];  
-            }
+                $resultDKey = mysqli_stmt_get_result($stmtDKeys);
+                
+                $resultDKeyCheck = mysqli_num_rows($resultDKey);
+                
+                $holdDaddr = -1;
+                
+                if($resultDKeyCheck > 0){
+                    while($resultDKeyCheck = mysqli_fetch_assoc($resultDKey))
+                    {
+                        $holdDaddr = $resultDKeyCheck["Address_Key"];       
+                    }
+                } else {
+                    //New (destination) Address tuple
+                    $sqlDaddr = "INSERT INTO PostOffice.Address (Building_Num, Street_Name, City, State, Zipcode)
+                    VALUES (?, ?, ?, ?, ?);";
+                    $stmtDaddr = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmtDaddr, $sqlDaddr)) {
+                        header("location: ../pages/index-login.php?error=stmtfailed");
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmtDaddr, "isssi", $Dbnum_converted, $Dstreet, $Dcity, $Dstate, $Dzipcode_coverted);
+                    mysqli_stmt_execute($stmtDaddr);
+                    //Retrieve Destination Destination Address Keys
+                    $addrDKeys = "SELECT Address_Key FROM PostOffice.Address 
+                                WHERE Building_Num = ? AND Street_Name = ? AND City = ? AND State = ? AND Zipcode = ?;";
+                
+                    $stmtDKeys = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmtDKeys, $addrDKeys)) {
+                        header("location: ../pages/index-login.php?error=stmtfailed");
+                        exit();   
+                    }
+                    mysqli_stmt_bind_param($stmtDKeys, "isssi", $Dbnum_converted, $Dstreet, $Dcity, $Dstate, $Dzipcode_coverted );
+                    mysqli_stmt_execute($stmtDKeys);
 
-            
-        }
-
-        else {
-            header("location: ../pages/emp-create-pkg-1.php?error=packagedoesnotexist");
-            exit();
-        }
-        
-        //Get Incoming Address
-        $addrSql = "SELECT Building_Num, Street_Name, City, State, Zipcode FROM PostOffice.Address, PostOffice.Package WHERE Address.Address_Key = Package.IC_Address_Key;";
-        $resultAddrRow = mysqli_query($conn, $addrSql);
-        $resultAddrRowCheck = mysqli_num_rows($resultAddrRow);
-        
-        $holdAddrBnum = -1;
-        $holdStreet = "";
-        $holdCity = "";
-        $holdState = "";
-        $holdZip = -1;
-        
-        if($resultAddrRowCheck > 0){
-            while($resultAddrRowCheck = mysqli_fetch_assoc($resultAddrRow))
-            {
-                $holdAddrBnum = $resultAddrRowCheck['Building_Num'];
-                $holdStreet = $resultAddrRowCheck['Street_Name'];
-                $holdCity = $resultAddrRowCheck['City'];
-                $holdState = $resultAddrRowCheck['State'];
-                $holdZip = $resultAddrRowCheck['Zipcode'];
-            }
-
-            
-        //Get Destination  Address
-        $addrSql2 = "SELECT Building_Num, Street_Name, City, State, Zipcode FROM PostOffice.Address, PostOffice.Package WHERE Address.Address_Key = Package.OT_Address_Key;";
-        $resultAddrRow2 = mysqli_query($conn, $addrSql2);
-        $resultAddrRowCheck2 = mysqli_num_rows($resultAddrRow2);
-        
-        $holdDAddrBnum = -1;
-        $holdDStreet = "";
-        $holdDCity = "";
-        $holdDState = "";
-        $holdDZip = -1;
-        
-        if($resultAddrRowCheck2 > 0){
-            while($resultAddrRowCheck2 = mysqli_fetch_assoc($resultAddrRow2))
-            {
-                $holdDAddrBnum = $resultAddrRowCheck2['Building_Num'];
-                $holdDStreet = $resultAddrRowCheck2['Street_Name'];
-                $holdDCity = $resultAddrRowCheck2['City'];
-                $holdDState = $resultAddrRowCheck2['State'];
-                $holdDZip = $resultAddrRowCheck2['Zipcode'];
-            }
-
-        }
-        } //VERY SUSSY CLOSING BRACKET, IF THIS PAGE IS BROKEN SOMEHOW THIS MIGHT BE THE CULPRIT!
-        ?>
-
+                    $resultDKey = mysqli_stmt_get_result($stmtDKeys);
+                
+                    $resultDKeyCheck = mysqli_num_rows($resultDKey);
+                
+                    $holdDaddr = -1;
+                
+                    if($resultDKeyCheck > 0) {
+                        while($resultDKeyCheck = mysqli_fetch_assoc($resultDKey)) {
+                            $holdDaddr = $resultDKeyCheck["Address_Key"];       
+                        }
+                    }
+                    else {
+                        echo "error";
+                    }
+                }
+            ?>
             <div class="form-col">
                 <div>
                     <i class="fa fa-dropbox" aria-hidden="true"></i>
                     <span>
-                        <h5>Package Info For Customer <?php echo $holdPkgKey; ?></h5>
-                    </span>
-                </div>
-        
-                <div>
-                    <span>
-                        <h2>Package Sent From</h2>
+                        <h5>Package Info For Johnny</h5>
                     </span>
                 </div>
 
                 <div>
                     <span>
-                        <p id="display-info">
-                            <?php
-                                echo $holdAddrBnum . " " . $holdStreet . " " . $holdCity . " " . $holdState . " " . $holdZip;
-                            ?>
-
-                        </p>
+                        <h2>Package Sent</h2>
                     </span>
                 </div>
 
-                <div>
-                    <span>
-                        <h2>Package Information</h2>
-                    </span>
-                </div>
-
-                
-                <table class="content-table">
-                    <thead>
-                        <tr>
-                            <th>Package ID</th>
-                            <th>Destination Address</th>
-                            <th>Price</th>
-                            <th>State</th>
-                            <th>Zipcode</th>
-                            <th>Package Weight (lbs)</th>
-                            <th>Package Volume (lbs^3)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?php echo $holdPkgKey; ?></td>
-                            <td><?php echo $holdDAddrBnum . " " . $holdDStreet . "," . $holdDCity;  ?></td>
-                            <td><?php echo "hewo"; ?></td>
-                            <td><?php echo $holdDState; ?></td>
-                            <td><?php echo $holdDZip ?></td>
-                            <td>30</td>
-                            <td>40</td> 
-                        </tr>
-                    </tbody>
-                </table>
-
-                
                 <form method="post" action="emp-create-pkg-3.php">
-                    <div>
-                        <span>
-                            <h2>Current Post Office</h2>
-                        </span>
-                    </div>
-                    <p id="display-info">   
-                            <?php
-                                if (isset($_SESSION["officeID"]))
-                                {
-                                    if ($_SESSION["officeID"] === 1){
-                                        echo "Houston Branch";
-                                    }
-                                }
-                                if (isset($_SESSION["officeID"]))
-                                {
-                                    if ($_SESSION["officeID"] === 3){
-                                        echo "Austin Branch";
-                                    }
-                                }
-                                if (isset($_SESSION["officeID"]))
-                                {
-                                    if ($_SESSION["officeID"] === 4){
-                                        echo "Dallas Branch";
-                                    }
-                                }
-                                else {
-                                    echo "No PO found.";
-                                }
-                            ?>
-                        </p>
-                    
-                    <div>
-                        <span>
-                            <h2>Send To Next Post Office</h2>
-                        </span>
-                    </div>
-                    <input type="text" name="nextPO" placeholder="Enter new post office to ship to" required>
-              
-                    <button type="submit" class="hero-btn red-btn" id="create-pkg-send-to">Send</button>
-
+                <button type="submit" class="hero-btn red-btn" id="create-pkg-send-to">Send To Next Post Office</button>
+                <?php
+                    $notReceived = 0;
+                    //Create Package
+                    $newPkg = "INSERT INTO PostOffice.Package (Customer_ID, Package_Type, Package_Weight, Package_Volume, IC_Address_Key, OT_Address_Key, Recieved)
+                            VALUES(?,?,?,?,?,?,?);";
+                    $stmtPkg = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmtPkg, $newPkg)) {
+                        header("location: ../pages/index-login.php?error=stmtfailed");
+                        exit();
+                    }
+                    mysqli_stmt_bind_param($stmtPkg, "isddiii", $customID, $ptype, $weight, $vol_converted, $holdRaddr,$holdDaddr, $notReceived);
+                    mysqli_stmt_execute($stmtPkg);
+                ?>
                 <!-- TODO: can't remove: will mess up side bar, so just hide by using color white in css -->
                 <p class="heading"> HEADING </p>
                 <p class="paragraph"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus blanditiis cumque voluptate laboriosam? Voluptate delectus saepe impedit, dolores aliquam in possimus corporis rerum a quam itaque dolor animi cupiditate expedita.</p>
             </div> 
-            
+
         </div>
 
     </section>
-    
+
 </body>
-</html>    
+</html>
--- root/pages/emp-create-pkg-3.php
+++ root/pages/emp-create-pkg-3.php
@@ -1,30 +1,6 @@
-<?php
-    include("../includes/dbh.inc.php");
-?>
-
-<section class="header">
-    <?php
-    include_once '../header.php';
-    ?>
-</section>
-
 <?php 
     // collect information from where they are sending the package to
 
-    $publish_date = date("Y-m-d H:i:s");
-    $sqlSend1 = "INSERT INTO Tracking (Package_ID, DateArrived, DateSent, Tracking_Office_ID)
-                    VALUES (?,?,?,?);";
-    $stmtSend1 = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmtSend1, $sqlSend1))
-    {
-        header("location: ../pages/index-login.php?error=stmtfailed");
-        exit();
-    }
-    mysqli_stmt_bind_param($stmtSend1, "issi", $_SESSION['packageIDglobal'], $publish_date, $publish_date, $_SESSION["officeID"]);
-    mysqli_stmt_execute($stmtSend1);
-
-    //Destroys session variable
-    unset($_SESSION['packageIDglobal']);
 
     // redirect to emp-create-pkg-1.php incase they'd like to repeat all of this with a new user.
     echo "<script> location.href='emp-create-pkg-1.php'; </script>";
